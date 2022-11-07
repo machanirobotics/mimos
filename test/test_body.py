8c3952c63a8cfa9f6e39a3e0ef9627ce6d5bc69f
rohanshetty-dev <rohanshetty@machanirobotics.com>
2022-11-07 14:37:55 +0530

    added sample.blend file

--- test/test_body.py
+++ test/test_body.py
@@ -6,12 +6,12 @@ def test_create_body():
     # pass in wrong file path
     with pytest.raises(FileNotFoundError):
         body = mi.Body(
-            skeleton=mi.skeleton.Blender("blendfiles/Ria2.blend", debug=True),
+            skeleton=mi.skeleton.Blender("blendfiles/sample2.blend", debug=True),
         )
         assert body is None
 
     # pass in correct file path
     body = mi.Body(
-        skeleton=mi.skeleton.Blender("blendfiles/Ria.blend", debug=True),
+        skeleton=mi.skeleton.Blender("blendfiles/sample.blend", debug=True),
     )
     assert body is not None
